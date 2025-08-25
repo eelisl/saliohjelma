2e8d86a864ae6a5d9f258c6ac239e1fd6953e0a6
eelis <dev@sovian.fi>
2025-08-25 21:24:00 +0300

    Use index.html as template for root

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask, render_template
 from dotenv import load_dotenv
 import os
 
@@ -8,7 +8,7 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello world!'
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
