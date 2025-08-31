228d5ce85c1b3a26e3d0ef04e74f55462d3e63fa
eelis <dev@sovian.fi>
2025-08-31 13:55:51 +0300

    feature: initialise database initialisation with files, populate schema

--- run-app.sh
+++ run-app.sh
@@ -8,8 +8,28 @@ fi
 
 pip install -r requirements.txt
 
-if [[ "$1" == "--dev" ]]; then
-    export FLASK_DEBUG=1
-fi
+for arg in "$@"; do
+    case $arg in
+        --dev)
+            export FLASK_DEBUG=1
+            echo "Debug enabled."
+            ;;
+        --init)
+            if [ -f ./database.db ]; then
+                echo "Database found. Delete existing database first to initialise new."
+                continue
+            fi
+            if [ ! -f ./database/schema.sql ]; then
+                echo "Schema file ./database/schema.sql not found."
+                exit 1
+            fi
+            if [ ! -d ./database ]; then
+                mkdir -p ./database
+            fi
+            sqlite3 database.db < ./database/schema.sql
+            echo "Database initialised."
+            ;;
+    esac
+done
 
 flask run
